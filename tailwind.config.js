07cc0cedbf1028d7b711b70876b51c14ed77ad31
AndreyTrolezi <laureano.yane@gmail.com>
2024-08-07 23:17:44 -0300

    projeto da primeira agina ta indo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      "Borda": "#1A2D7A",
+      "SubTitulo": "#1465BB",
+      "CorSubTitulo": "#81C9FA",
+      "bt": "#1465BB",
+      "Titulo": "#1A2D7A",
+      "Letras": "#1A2D7A",
+    },
   },
   plugins: [],
 }
-
