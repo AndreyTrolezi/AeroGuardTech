bb35471bbcc6761fbb76770610f862f6e9eb1efb
AndreyTrolezi <laureano.yane@gmail.com>
2024-08-13 00:36:21 -0300

    bob quase finalizou a primeira pagina, só n terminou pq foi dormir

--- src/pages/Pagina1.jsx
+++ src/pages/Pagina1.jsx
@@ -12,18 +12,19 @@ export function Pagina1() {
       </header>
       <main>
         <section class="info">
-          <h2 class="Subtitulo">Sobre o Produto</h2>
-          <p class="Descricao">Apresentamos nossa solução inovadora para problemas respiratórios, projetada para transformar a maneira como você cuida da sua saúde. Junte-se a nós nesta jornada de avanço tecnológico em prol de uma melhor qualidade de vida.</p>
+          <h2 class="Subtitulo1">Sobre o Produto</h2>
+          <p class="Descricao1">Apresentamos nossa solução inovadora para problemas respiratórios, projetada para transformar a maneira como você cuida da sua saúde. Junte-se a nós nesta jornada de avanço tecnológico em prol de uma melhor qualidade de vida.</p>
         </section>
         <section>
           <h2 class="Subtitulo">Público alvo</h2>
           <p class="Descricao">Nosso produto é feito especificamente para pessoas com problemas respiratórios, sejam elas crianças, adultos ou idosos. Fizemos pensando em dar uma melhor qualidade de vida para todos.</p>
         </section>
+        <h2>Saiba Mais</h2>{/*tem isso aq pra editar bob, ele é um h2 q fica encima das ancoras q levam pra outras paginas*/}
         <div>
-          <a href="/sobre" >Sobre nós</a> {/*precisa do msm nome q ta no path pro bglh aq funcionar, como la ta /teste, tem q colocar /teste aq pq senao da erro*/}
-          <a href="/mate" >Materiais</a>
-          <a href="/refs" >Referências</a>
-          <a href="/guia" >Guia</a>
+          <a class="link1" href="/sobre" >Sobre nós</a> {/*precisa do msm nome q ta no path pro bglh aq funcionar, como la ta /teste, tem q colocar /teste aq pq senao da erro*/}
+          <a class="link1"href="/mate" >Materiais</a>
+          <a class="link1"href="/refs" >Referências</a>
+          <a class="link1"href="/guia" >Guia</a>
         </div>
       </main>
     </Home>
--- src/styles/pagina1.styles.js
+++ src/styles/pagina1.styles.js
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 
 export const Home = styled.div`
   background-color: #ffffff;
-  border: 18px solid #1A2D7A;
-  padding: 40px;
+  border: 8px solid #1A2D7A;
+  padding: 60px;
   text-align: center;
-  max-width: auto;
-  margin: 0 auto;
+  max-width: 100%;
+  
  
   .Logo {
     font-size: 2rem;
     color: #0a0a78;
-    margin-bottom: 20px;
+    margin-bottom: 5px;
 
     img {
       max-width: 200px;
@@ -24,6 +24,7 @@ export const Home = styled.div`
     font-size: 3rem;
     color: #0a0a78;
     margin-bottom: 20px;
+    padding-bottom: 20px;
   }
 
   .Subtitulo {
@@ -31,12 +32,48 @@ export const Home = styled.div`
     font-size: 2rem;
     color: #81C9FA;
     margin-bottom: 10px;
+    margin-left: 30rem;
+    margin-right: 30rem;
+    border-radius: 5px;
+  }
+
+  .Subtitulo1 {
+    background-color: #1465BB;
+    font-size: 2rem;
+    color: #81C9FA;
+    margin-bottom: 10px;
+    margin-left: 28rem;
+    margin-right: 28rem;
+    border-radius: 5px;
   }
 
   .Descricao {
-    font-size: 1rem;
+    font-size: 1.2rem;
     color: #000;
     margin-bottom: 20px;
+    margin-left: 15rem;
+    margin-right: 15rem;
+  }
+
+  .Descricao1 {
+    font-size: 1.2rem;
+    color: #000;
+    margin-bottom: 20px;
+    margin-left: 15rem;
+    margin-right: 15rem;
+  }
+
+  .link1 {
+    background-color: #1465BB;
+    font-size: 1.5rem;
+    color: #ffffff;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    padding-left: 5px;
+
+    &:hover {
+      background-color: #005bb5;
+  }
   }
 
   ;
