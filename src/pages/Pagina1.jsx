eb1c5a7c953c866d7bd5bec3d614ce7a81450566
AndreyTrolezi <laureano.yane@gmail.com>
2024-08-12 23:20:04 -0300

    inicio da estilização da primeira pagina uhuuuul(só deus sabe como ta a nossa mente nesse exato momento)

--- src/pages/Pagina1.jsx
+++ src/pages/Pagina1.jsx
@@ -1,21 +1,22 @@
-import Logo from "../assets/logo.png"
+import logo from "../assets/logo.png"
+import { Home } from "../styles/pagina1.styles"//EDUZÃO TA PRA GAME
 
 export function Pagina1() {
   return (
-    <div>
+    <Home>
       <header>
-        <div >
-          <img src={Logo} alt="" />
+        <div class="Logo">
+          <img src={logo} alt="" />
         </div>
-        <h1>AeroGuard Tech</h1>
+        <h1 class="Title">AeroGuard Tech</h1>
       </header>
       <main>
         <section>
-          <h2 >Sobre o Produto</h2>
+          <h2 class="Subtitle">Sobre o Produto</h2>
           <p>Apresentamos nossa solução inovadora para problemas respiratórios, projetada para transformar a maneira como você cuida da sua saúde. Junte-se a nós nesta jornada de avanço tecnológico em prol de uma melhor qualidade de vida.</p>
         </section>
         <section>
-          <h2 >Público alvo</h2>
+          <h2 class="Subtitle">Público alvo</h2>
           <p >Nosso produto é feito especificamente para pessoas com problemas respiratórios, sejam elas crianças, adultos ou idosos. Fizemos pensando em dar uma melhor qualidade de vida para todos.</p>
         </section>
         <div>
@@ -25,6 +26,6 @@ export function Pagina1() {
           <a href="/guia" >Guia</a>
         </div>
       </main>
-    </div>
+    </Home>
   );
 }
--- src/styles/pagina1.styles.js
+++ src/styles/pagina1.styles.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components';
 
 
+export const Home = styled.div`
+  background-color: #ffffff;
+  border: 18px solid #1A2D7A;
+  padding: 40px;
+  text-align: center;
+  max-width: auto;
+  margin: 0 auto;
+ 
+  .Logo {
+    font-size: 2rem;
+    color: #0a0a78;
+    margin-bottom: 20px;
+
+    .Logo img {
+      max-width: 200px;
+      max-height: 150px;
+    }
+  }
+
+  .Title {
+    font-size: 1.5em;
+    color: #0a0a78;
+    margin-bottom: 20px;
+  }
+
+  
+`
